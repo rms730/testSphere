b93df30b273eeced23a5f428f757eba6209119a8
Randy Sternke <rms730@gmail.com>
2020-04-16 20:59:50 -0700

    Initial Commit

---
+++ TestSphere/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSphere
+//
+//  Created by Randy Sternke on 4/16/20.
+//  Copyright © 2020 Randy Sternke. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
